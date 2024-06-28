a43fa5741dee0ff281a3fdb5ba5dd641774f72b3
Sivan wolberg <sivan@wolberg.pro>
2024-06-28 20:49:45 +0300

    update connections

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,6 @@
 import "server-only";
 
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { sql } from '@vercel/postgres';
+import { drizzle } from 'drizzle-orm/vercel-postgres';
 
-export const db = drizzle(
-  neon(process.env.DATABASE_URL!, {
-    fetchOptions: {
-      cache: "no-store",
-    },
-  })
-);
+export const db = drizzle(sql)
