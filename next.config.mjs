8e5a62df29c6c06677ad4361ebea4219e6808361
Sivan wolberg <sivan@wolberg.pro>
2024-07-06 02:07:08 +0300

    update code base

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,9 @@ const nextConfig = {
         missingSuspenseWithCSRBailout: false,
     },
 
+    experimental: {
+        instrumentationHook: true,
+    },
     transpilePackages: ['lucide-react'],
     reactStrictMode: true,
     images: {
