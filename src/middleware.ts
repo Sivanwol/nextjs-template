83799e9d434c252f83f2f0797b56b78bd25416f2
Sivan wolberg <sivan@wolberg.pro>
2024-07-11 15:31:37 +0300

    fix: formatting

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import createMiddleware from "next-intl/middleware";
 export default createMiddleware({
   // A list of all locales that are supported
   locales,
-  localePrefix: 'as-needed',
+  localePrefix: "as-needed",
   defaultLocale: "en",
 });
 
