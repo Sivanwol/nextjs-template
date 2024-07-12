be0ad80cb4fce61a086ec200717b15d718190892
Sivan wolberg <sivan@wolberg.pro>
2024-07-12 18:07:57 +0300

    fix: more work and fix

--- src/components/header/segments/headerNavBar.tsx
+++ src/components/header/segments/headerNavBar.tsx
@@ -2,13 +2,8 @@ import { Navbar, NavbarLink } from "flowbite-react";
 import { HeaderBrand } from "./headerBrand";
 import { useTranslations } from "next-intl";
 import { HeaderSelectLanguage } from "./headerSelectLanguage";
-import { Pages } from "@app/lib/types";
 
-export function HeaderNavBar({
-  activePage,
-}: Readonly<{
-  activePage: Pages;
-}>) {
+export function HeaderNavBar() {
   const t = useTranslations("common");
   return (
     <>
@@ -16,21 +11,11 @@ export function HeaderNavBar({
         <HeaderBrand />
         <Navbar.Toggle />
         <Navbar.Collapse>
-          <NavbarLink href="#" active={activePage === Pages.MyServices}>
-            {t("menu.myServices")}
-          </NavbarLink>
-          <NavbarLink href="#" active={activePage === Pages.MyCv}>
-            {t("menu.myCV")}
-          </NavbarLink>
-          <NavbarLink href="#" active={activePage === Pages.About}>
-            {t("menu.aboutme")}
-          </NavbarLink>
-          <NavbarLink href="#" active={activePage === Pages.MyWork}>
-            {t("menu.mywork")}
-          </NavbarLink>
-          <NavbarLink href="#" active={activePage === Pages.Contact}>
-            {t("menu.contact")}
-          </NavbarLink>
+          <NavbarLink href="#">{t("menu.myServices")}</NavbarLink>
+          <NavbarLink href="#">{t("menu.myCV")}</NavbarLink>
+          <NavbarLink href="#">{t("menu.aboutme")}</NavbarLink>
+          <NavbarLink href="#">{t("menu.mywork")}</NavbarLink>
+          <NavbarLink href="#">{t("menu.contact")}</NavbarLink>
           <div className="flex md:order-2 ml-3 mr-3">
             <HeaderSelectLanguage />
           </div>
