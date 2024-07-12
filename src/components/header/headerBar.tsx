fb29a3e4dce7b95b56030150167a7025ff29e9e9
Sivan wolberg <sivan@wolberg.pro>
2024-07-12 18:12:30 +0300

    fix: this crap

--- src/components/header/headerBar.tsx
+++ src/components/header/headerBar.tsx
@@ -1,8 +1,4 @@
-"use client";
 import { HeaderNavBar } from "./segments/headerNavBar";
-import { usePathname } from "next/navigation";
-import { determinePage } from "@app/lib/utils";
 export function HeaderBar() {
-  const pathname = usePathname();
-  return <HeaderNavBar activePage={determinePage(pathname)} />;
+  return <HeaderNavBar />;
 }
