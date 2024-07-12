630c79cdd0536da121b7edf64bf206539c76a5e1
Sivan wolberg <sivan@wolberg.pro>
2024-07-12 16:07:10 +0300

    fix: testing

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,3 @@
-import PageEffects from "@app/components/pageEffects";
-
 export default function Homepage() {
-  return (
-    <PageEffects>
-      <h1>Home Page</h1>
-    </PageEffects>
-  );
+  return <h1>Home Page</h1>;
 }
