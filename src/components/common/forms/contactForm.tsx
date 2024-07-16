35c55ae3b69f2ad6ea1b4e095c6bf64b30f97697
Sivan wolberg <sivan@wolberg.pro>
2024-07-16 03:54:16 +0300

    chore: update form

---
+++ src/components/common/forms/contactForm.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+import { useFormik } from "formik";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { ButtonGroup } from "@mui/material";
+
+import {
+  ContactFormInitialValues,
+  ContactFormValues,
+  ContactValidationFormSchema,
+} from "./schemas/contactValidationForm";
+import { useFormState, useFormStatus } from "react-dom";
+import { sentEmail } from "@app/lib/server/actions/mail";
+import { HiInformationCircle } from "react-icons/hi";
+import { Alert } from "flowbite-react";
+
+export default function ContactForm() {
+  const t = useTranslations("homepage");
+  const { pending } = useFormStatus();
+  const [state, formAction] = useFormState(sentEmail, ContactFormInitialValues);
+  const {
+    values,
+    errors,
+    touched,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    resetForm,
+  } = useFormik({
+    initialValues: ContactFormInitialValues,
+    validationSchema: ContactValidationFormSchema(t),
+    onSubmit: (values) => {
+      const formData: FormData = new FormData();
+      for (const [key, value] of Object.entries(values)) {
+        formData.append(key, value);
+      }
+      formAction(formData);
+    },
+  });
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="text-sm font-medium text-appRed-500">
+        {t("contactSegment.title")}
+      </h2>
+      <p className="text-sm font-medium text-gray-500">
+        {t("contactSegment.extraText")}
+      </p>
+      <TextField
+        fullWidth
+        id="name"
+        name="name"
+        variant="outlined"
+        className="mt-2"
+        placeholder={t("contactSegment.contactForm.placeHolder.name")}
+        label={t("contactSegment.contactForm.name")}
+        value={values.name}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.name && Boolean(errors.name)}
+        helperText={touched.name && errors.name}
+      />
+      <TextField
+        fullWidth
+        id="email"
+        name="email"
+        variant="outlined"
+        className="mt-2"
+        placeholder={t("contactSegment.contactForm.placeHolder.email")}
+        label={t("contactSegment.contactForm.email")}
+        value={values.email}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.email && Boolean(errors.email)}
+        helperText={touched.email && errors.email}
+      />
+      <TextField
+        fullWidth
+        id="phone"
+        name="phone"
+        variant="outlined"
+        className="mt-2"
+        placeholder={t("contactSegment.contactForm.placeHolder.phone")}
+        label={t("contactSegment.contactForm.phone")}
+        value={values.phone}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.phone && Boolean(errors.phone)}
+        helperText={touched.phone && errors.phone}
+      />
+      <TextField
+        fullWidth
+        id="subject"
+        name="subject"
+        variant="outlined"
+        className="mt-2"
+        placeholder={t("contactSegment.contactForm.placeHolder.subject")}
+        label={t("contactSegment.contactForm.subject")}
+        value={values.subject}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.subject && Boolean(errors.subject)}
+        helperText={touched.subject && errors.subject}
+      />
+
+      <TextField
+        fullWidth
+        id="message"
+        name="message"
+        variant="outlined"
+        placeholder={t("contactSegment.contactForm.placeHolder.message")}
+        className="mt-2"
+        multiline
+        maxRows={4}
+        label={t("contactSegment.contactForm.message")}
+        value={values.message}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.message && Boolean(errors.message)}
+        helperText={touched.message && errors.message}
+      />
+      {state ? (
+        <Alert color="failure" icon={HiInformationCircle} className="mt-3 mb-3">
+          <span className="font-medium pr-3">Sent Failed!</span>
+          {state?.message}
+        </Alert>
+      ) : null}
+      <div className="mt-4 flex flex-col content-end">
+        <ButtonGroup size="large" aria-label="Large button group">
+          <Button
+            color="primary"
+            variant="contained"
+            type="submit"
+            disabled={pending}
+            className="soft-shadow-blue hover:soft-shadow-blue w-[150px] mx-auto"
+          >
+            {t("contactSegment.contactForm.submit")}
+          </Button>
+
+          <Button
+            color="error"
+            variant="contained"
+            type="reset"
+            disabled={pending}
+            onClick={() => resetForm()}
+            className="soft-shadow-red hover:soft-shadow-red w-[150px] mx-auto"
+          >
+            {t("contactSegment.contactForm.reset")}
+          </Button>
+        </ButtonGroup>
+      </div>
+    </form>
+  );
+}
--- src/components/segments/homepage/contact.tsx
+++ src/components/segments/homepage/contact.tsx
@@ -1,87 +1,15 @@
 "use client";
+
 import { HR } from "flowbite-react";
 import { useTranslations } from "next-intl";
-import { useFormik } from "formik";
-import * as yup from "yup";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import { ButtonGroup } from "@mui/material";
+import ContactForm from "@app/components/common/forms/contactForm";
+import { sentEmail } from "@app/lib/server/actions/mail";
+import { ContactFormInitialValues } from "@app/components/common/forms/schemas/contactValidationForm";
+import { useFormState } from "react-dom";
 
 export default function ContactHP() {
   const t = useTranslations("homepage");
-  const validationSchema = yup.object({
-    email: yup
-      .string()
-      .email(t("contactSegment.contactForm.validations.email"))
-      .required(
-        t("contactSegment.contactForm.validations.required", {
-          fieldName: "Email",
-        }),
-      ),
 
-    name: yup
-      .string()
-      .email(t("contactSegment.contactForm.validations.name"))
-      .required(
-        t("contactSegment.contactForm.validations.required", {
-          fieldName: "Name",
-        }),
-      ),
-    phone: yup.string().required(
-      t("contactSegment.contactForm.validations.required", {
-        fieldName: "Phone",
-      }),
-    ),
-    subject: yup
-      .string()
-      .min(
-        3,
-        t("contactSegment.contactForm.validations.minLength", { minLength: 3 }),
-      )
-      .max(
-        120,
-        t("contactSegment.contactForm.validations.maxLength", {
-          maxLength: 120,
-        }),
-      )
-      .required(
-        t("contactSegment.contactForm.validations.required", {
-          fieldName: "Subject",
-        }),
-      ),
-    message: yup
-      .string()
-      .min(
-        3,
-        t("contactSegment.contactForm.validations.minLength", { minLength: 3 }),
-      )
-      .required(
-        t("contactSegment.contactForm.validations.required", {
-          fieldName: "Message",
-        }),
-      ),
-  });
-  const {
-    values,
-    errors,
-    touched,
-    handleBlur,
-    handleChange,
-    handleSubmit,
-    resetForm,
-  } = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      phone: "",
-      subject: "",
-      message: "",
-    },
-    validationSchema: validationSchema,
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
   return (
     <section className="border-2 border-appRed-500 px-3 sm:px-10 py-5 ml-5 mr-5 sm:py-10 mx-auto text-center mt-5 w-fit">
       <span
@@ -102,109 +30,7 @@ export default function ContactHP() {
       </div>
 
       <div className="flex flex-row items-center md:flex-row justify-between mt-4 gap-y-8r w-4xl">
-        <form onSubmit={handleSubmit}>
-          <h2 className="text-sm font-medium text-appRed-500">
-            {t("contactSegment.title")}
-          </h2>
-          <p className="text-sm font-medium text-gray-500">
-            {t("contactSegment.extraText")}
-          </p>
-          <TextField
-            fullWidth
-            id="name"
-            name="name"
-            variant="outlined"
-            className="mt-2"
-            placeholder={t("contactSegment.contactForm.placeHolder.name")}
-            label={t("contactSegment.contactForm.name")}
-            value={values.name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            error={touched.name && Boolean(errors.name)}
-            helperText={touched.name && errors.name}
-          />
-          <TextField
-            fullWidth
-            id="email"
-            name="email"
-            variant="outlined"
-            className="mt-2"
-            placeholder={t("contactSegment.contactForm.placeHolder.email")}
-            label={t("contactSegment.contactForm.email")}
-            value={values.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            error={touched.email && Boolean(errors.email)}
-            helperText={touched.email && errors.email}
-          />
-          <TextField
-            fullWidth
-            id="phone"
-            name="phone"
-            variant="outlined"
-            className="mt-2"
-            placeholder={t("contactSegment.contactForm.placeHolder.phone")}
-            label={t("contactSegment.contactForm.phone")}
-            value={values.phone}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            error={touched.phone && Boolean(errors.phone)}
-            helperText={touched.phone && errors.phone}
-          />
-          <TextField
-            fullWidth
-            id="subject"
-            name="subject"
-            variant="outlined"
-            className="mt-2"
-            placeholder={t("contactSegment.contactForm.placeHolder.subject")}
-            label={t("contactSegment.contactForm.subject")}
-            value={values.subject}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            error={touched.subject && Boolean(errors.subject)}
-            helperText={touched.subject && errors.subject}
-          />
-
-          <TextField
-            fullWidth
-            id="message"
-            name="message"
-            variant="filled"
-            placeholder={t("contactSegment.contactForm.placeHolder.message")}
-            className="mt-2"
-            multiline
-            maxRows={4}
-            label={t("contactSegment.contactForm.message")}
-            value={values.message}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            error={touched.message && Boolean(errors.message)}
-            helperText={touched.message && errors.message}
-          />
-          <div className="mt-3 flex flex-col content-end">
-            <ButtonGroup size="large" aria-label="Large button group">
-              <Button
-                color="primary"
-                variant="contained"
-                type="submit"
-                className="soft-shadow-blue hover:soft-shadow-blue w-[150px] mx-auto"
-              >
-                {t("contactSegment.contactForm.submit")}
-              </Button>
-
-              <Button
-                color="error"
-                variant="contained"
-                type="reset"
-                onClick={() => resetForm()}
-                className="soft-shadow-red hover:soft-shadow-red w-[150px] mx-auto"
-              >
-                {t("contactSegment.contactForm.reset")}
-              </Button>
-            </ButtonGroup>
-          </div>
-        </form>
+        <ContactForm />
       </div>
     </section>
   );
