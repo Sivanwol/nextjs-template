e2ed4561136c3cfa3321c1ee510e8a981e1003ba
Sivan wolberg <sivan@wolberg.pro>
2024-07-26 21:54:43 +0300

    revert: not only didnt work it was stupid

--- src/components/common/game/GameWrapper.tsx
+++ src/components/common/game/GameWrapper.tsx
@@ -1,22 +1,22 @@
-import { useEffect } from "react";
-import RunGame from "./Game";
+import { useState, useRef } from "react";
+import { IRefPhaserGame, PhaserGame } from "./PhaserGame";
 
 const GameWrapper = ({ config }: { config: Phaser.Types.Core.GameConfig }) => {
-  const refElem = "game-container";
+  // The sprite can only be moved in the MainMenu Scene
+  const [canMoveSprite, setCanMoveSprite] = useState(true);
+  //  References to the PhaserGame component (game and scene are exposed)
+  const phaserRef = useRef<IRefPhaserGame | null>(null);
+  // Event emitted from the PhaserGame component
+  const currentScene = (scene: Phaser.Scene) => {};
 
-  const loadGame = async () => {
-    if (typeof window !== "object") {
-      return;
-    }
-
-    const game = RunGame(refElem, config);
-    game.scene.start("BootScene");
-    return;
-  };
-  useEffect(() => {
-    loadGame().catch(console.error);
-  }, [loadGame]);
-
-  return <div id={refElem}></div>;
+  return (
+    <div id="wrapper">
+      <PhaserGame
+        ref={phaserRef}
+        config={config}
+        currentActiveScene={currentScene}
+      />
+    </div>
+  );
 };
 export default GameWrapper;
