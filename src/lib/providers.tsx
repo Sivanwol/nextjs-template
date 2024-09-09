ca647a2f1ca0e25f3edc37a0c647a7a48060ad0c
Sivan wolberg <sivan@wolberg.pro>
2024-09-10 01:44:47 +0300

    chore: remove unused environment variables and deprecated code
    - Remove unused environment variables related to Hypertune integration
    - Delete deprecated getHypertune function
    - Remove HypertuneProvider from providers.tsx

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,28 +1,12 @@
 import React from "react";
-import getHypertune from "@app/lib/getHypertune";
 import { UserProvider } from "@app/lib/state/providers/user-store-provider";
 
-import { HypertuneProvider } from "@gen/hypertune.react";
 const Providers = async ({
   children,
 }: Readonly<{
   children: any;
 }>) => {
-  // const hypertune = await getHypertune();
-  // const serverDehydratedState = hypertune.dehydrate();
-  // const serverRootArgs = hypertune.getRootArgs();
-
-  return (
-    // <HypertuneProvider
-    //   createSourceOptions={{
-    //     token: process.env.NEXT_PUBLIC_HYPERTUNE_TOKEN!,
-    //   }}
-    //   dehydratedState={serverDehydratedState}
-    //   rootArgs={serverRootArgs}
-    // >
-    <UserProvider>{children}</UserProvider>
-    // </HypertuneProvider>
-  );
+  return <UserProvider>{children}</UserProvider>;
 };
 
 export default Providers;
